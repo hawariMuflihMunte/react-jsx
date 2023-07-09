e4225e0e2a48c61716f6e4b7bb651fb9f32f8b38
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-07-09 21:10:04 +0700

    Send `props` to class component

---
+++ stateful-component/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+class MyComponent extends React.Component {
+  render() {
+    return <p>Hello, {this.props.name}!</p>;
+  }
+}
+
+const root = createRoot(document.getElementById('root'));
+root.render(<MyComponent name="Dicoding" />);
