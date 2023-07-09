f070258e84b8ff308c730be64b567f2cca49b98b
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-07-09 19:01:59 +0700

    Import and render components

--- contacts-app/src/index.jsx
+++ contacts-app/src/index.jsx
@@ -1,6 +1,5 @@
 import { createRoot } from 'react-dom/client';
-
-const element = <h1>Hello, world!</h1>;
+import ContactApp from './ContactApp';
 
 const root = createRoot(document.getElementById('root'));
-root.render(element);
+root.render(<ContactApp />);
