7d854c84469f5aa7e8976c4bf128d462c1e51138
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-07-10 20:40:06 +0700

    Make query input mutable

--- personal-notes-starter/src/components/NotesSearch.jsx
+++ personal-notes-starter/src/components/NotesSearch.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
 
-export default function NotesSearch({ query }) {
-  return (
-    <section className="note-search">
-      <input
-        type="text"
-        placeholder="Cari catatan ..."
-        value={query}
-      />
-    </section>
-  );
+export default class NotesSearch extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      query: ''
+    };
+
+    this.onChangeEventHandler = this.onChangeEventHandler.bind(this);
+  }
+
+  onChangeEventHandler(event) {
+    this.setState(() => {
+      return {
+        query: event.target.value
+      };
+    });
+  }
+
+  render() {
+    return (
+      <section className="note-search">
+        <input
+          type="text"
+          placeholder="Cari catatan ..."
+          value={this.state.query}
+          onChange={this.onChangeEventHandler}
+        />
+      </section>
+    );
+  }
 }
