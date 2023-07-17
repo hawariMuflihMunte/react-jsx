6b9945e651cc168265565816002486c4dfffead2
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-07-17 20:38:41 +0700

    Create link navigator

--- contacts-app/src/index.jsx
+++ contacts-app/src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import PropTypes from 'prop-types';
 import ContactApp from './components/ContactApp';
 
 // styling
@@ -17,6 +18,30 @@ function FAQPage() {
   return <p>This is FAQ page</p>;
 }
 
+function Link({
+  target,
+  navigate,
+  children
+}) {
+  return (
+    <a
+      href={target}
+      onClick={(event) => {
+        event.preventDefault();
+        navigate(target);
+      }}
+    >
+      {children}
+    </a>
+  );
+}
+
+Link.propTypes = {
+  target: PropTypes.string.isRequired,
+  navigate: PropTypes.func.isRequired,
+  children: PropTypes.element.isRequired
+}
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -24,6 +49,16 @@ class App extends React.Component {
     this.state = {
       page: '/'
     };
+
+    this.navigate = this.navigate.bind(this);
+  }
+
+  navigate(target) {
+    this.setState(() => {
+      return {
+        page: target
+      };
+    });
   }
 
   render() {
