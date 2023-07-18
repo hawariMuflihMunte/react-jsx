5522e99e84aa3b72c171a23abd45e64d7e6066f1
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-07-18 11:46:49 +0700

    Revert project to Contact Apps

--- contacts-app/src/index.jsx
+++ contacts-app/src/index.jsx
@@ -1,76 +1,14 @@
-import React from 'react';
 import { createRoot } from 'react-dom/client';
 import PropTypes from 'prop-types';
-import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import ContactApp from './components/ContactApp';
 
 // styling
 import './style.css';
 
-function HomePage() {
-  return <p>This is Homepage</p>;
-}
-
-function AboutPage() {
-  return <p>This is About page</p>;
-}
-
-function FAQPage() {
-  return <p>This is FAQ page</p>;
-}
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      page: '/'
-    };
-
-    this.navigate = this.navigate.bind(this);
-  }
-
-  navigate(target) {
-    this.setState(() => {
-      return {
-        page: target
-      };
-    });
-  }
-
-  render() {
-    return (
-      <>
-        <header>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-              <li>
-                <Link to="/faq">FAQ</Link>
-              </li>
-            </ul>
-          </nav>
-        </header>
-        <main>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/faq" element={<FAQPage />} />
-          </Routes>
-        </main>
-      </>
-    );
-  }
-}
-
 const root = createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <App />
+    <ContactApp />
   </BrowserRouter>
 );
