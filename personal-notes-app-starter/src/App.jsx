43a272c92a703d066258eb9bc4699cac1571c994
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-07-23 01:08:35 +0700

    Init first page

--- personal-notes-app-starter/src/App.jsx
+++ personal-notes-app-starter/src/App.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import { BrowserRouter, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="app-container">
-      <h1>Hello, React</h1>
+      <header>
+        <h1>
+          <Link to="/">Aplikasi Catatan</Link>
+        </h1>
+        <nav className='navigation'>
+          <ul>
+            <li>
+              <Link to="/archives">Arsip</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <section className='homepage'>
+          <h2>Catatan Aktif</h2>
+          <section className='search-bar'>
+            <input type="text" placeholder='Cari berdasarkan judul...' />
+          </section>
+          <section className='notes-list'>
+            <article className='note-item'>
+              <h3 className='note-item__title'>
+                <a href="#">Lorem</a>
+              </h3>
+              <p className='note-item__createdAt'>Kamis, 14 April 2022</p>
+              <p className='note-item__body'>Lorem ipsum dolor sit amet.</p>
+            </article>
+          </section>
+        </section>
+      </main>
     </div>
   );
 }
