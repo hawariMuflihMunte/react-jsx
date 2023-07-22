e10066b73fdcea103136dbcc3cc324a1758504a5
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-07-23 02:54:58 +0700

    Receive and display data in detail page

--- personal-notes-app-starter/src/pages/DetailPage.jsx
+++ personal-notes-app-starter/src/pages/DetailPage.jsx
@@ -1,12 +1,16 @@
 import { useParams } from 'react-router-dom';
+import { getNote } from '../utils/local-data';
+import { showFormattedDate } from '../utils';
 
 export function DetailPage() {
   const { id } = useParams();
+  const note = getNote(id);
 
   return (
-    <>
-      <h1>Detail Page</h1>
-      <p>{id}</p>
-    </>
+    <section className='detail-page'>
+      <h2 className='detail-page__title'>{note.title}</h2>
+      <p className='detail-page__createdAt'>{showFormattedDate(note.createdAt)}</p>
+      <div className='detail-page__body'>{note.body}</div>
+    </section>
   )
 };
