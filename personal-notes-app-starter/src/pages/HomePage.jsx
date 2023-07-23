22405836ed8889f8cfd0ced98ca6a4b8b25b5a5a
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-07-23 12:45:21 +0700

    Re-import child components

--- personal-notes-app-starter/src/pages/HomePage.jsx
+++ personal-notes-app-starter/src/pages/HomePage.jsx
@@ -1,4 +1,6 @@
+import { ButtonAdd } from '../components/ButtonAdd';
 import { NoteItem } from '../components/NoteItem';
+import { SearchBar } from '../components/SearchBar';
 import { getAllNotes } from '../utils/local-data';
 
 export function HomePage() {
@@ -7,9 +9,7 @@ export function HomePage() {
   return (
     <section className='homepage'>
       <h2>Catatan Aktif</h2>
-      <section className='search-bar'>
-        <input type="text" placeholder='Cari berdasarkan judul ...' />
-      </section>
+      <SearchBar />
       <section className='notes-list'>
         {notes.map((note) => (
           <NoteItem
@@ -22,9 +22,7 @@ export function HomePage() {
         ))}
       </section>
       <div className='homepage__action'>
-        <button className='action' type='button' title='tambah'>
-          <svg stroke="currentColor" fill="currentColor" strokeWidth={0} viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0V0z" /><path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" /></svg>
-        </button>
+        <ButtonAdd />
       </div>
     </section>
   )
