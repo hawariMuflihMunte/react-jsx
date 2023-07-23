52dbf81e168f8c0cebf5abc83c1541e24278ba37
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-07-23 12:46:59 +0700

    Desctructure object datas and import buttons

--- personal-notes-app-starter/src/pages/DetailPage.jsx
+++ personal-notes-app-starter/src/pages/DetailPage.jsx
@@ -1,16 +1,26 @@
 import { useParams } from 'react-router-dom';
 import { getNote } from '../utils/local-data';
 import { showFormattedDate } from '../utils';
+import { ButtonArchive } from '../components/ButtonArchive';
+import { ButtonDelete } from '../components/ButtonDelete';
 
 export function DetailPage() {
   const { id } = useParams();
-  const note = getNote(id);
+  const {
+    title,
+    createdAt,
+    body
+  } = getNote(id);
 
   return (
     <section className='detail-page'>
-      <h2 className='detail-page__title'>{note.title}</h2>
-      <p className='detail-page__createdAt'>{showFormattedDate(note.createdAt)}</p>
-      <div className='detail-page__body'>{note.body}</div>
+      <h2 className='detail-page__title'>{title}</h2>
+      <p className='detail-page__createdAt'>{showFormattedDate(createdAt)}</p>
+      <div className='detail-page__body'>{body}</div>
+      <div className='detail-page__action'>
+        <ButtonArchive />
+        <ButtonDelete />
+      </div>
     </section>
   )
 };
